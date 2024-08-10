bdb59d33ec94918e7d958affe7041982e02c8efe
yxrael <yxrael@gmail.com>
2024-08-10 11:01:25 +0200

    exos

--- components/ExpensesOutput/ExpensesOutput.jsx
+++ components/ExpensesOutput/ExpensesOutput.jsx
@@ -3,62 +3,7 @@ import { ExpensesList } from "./ExpensesList"
 import { ExpensesSummary } from "./ExpensesSummary"
 import { GlobalStyles } from "../../constants/styles"
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2022-12-19')
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2022-01-05')
-  },
-  {
-    id: 'e3',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2021-12-01')
-  },
-  {
-    id: 'e4',
-    description: 'A book',
-    amount: 14.99,
-    date: new Date('2022-01-19')
-  },
-  {
-    id: 'e5',
-    description: 'Another book',
-    amount: 18.59,
-    date: new Date('2022-02-18')
-  },
-  {
-    id: 'e6',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2022-01-05')
-  },
-  {
-    id: 'e7',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2021-12-01')
-  },
-  {
-    id: 'e8',
-    description: 'A book',
-    amount: 14.99,
-    date: new Date('2022-01-19')
-  },
-  {
-    id: 'e9',
-    description: 'Another book',
-    amount: 18.59,
-    date: new Date('2022-02-18')
-  },
-]
+
 
 
 
